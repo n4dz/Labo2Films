651e7bbf77eb3765225f916b7618cc5e0ab733e0
LunaBeauchamp <116032544+LunaBeauchamp@users.noreply.github.com>
2023-10-23 13:17:04 -0400

    création classe film

---
+++ app/src/main/java/com/example/labo2films/Film.java
@@ -0,0 +1,68 @@
+package com.example.labo2films;
+
+import java.io.Serializable;
+
+public class Film implements Serializable {
+    private int num;
+    private String titre;
+    private int codeCateg;
+    private String langue;
+    private int cote;
+
+    public Film(Film film){
+        this.num = film.num;
+        this.titre = film.titre;
+        this.codeCateg = film.codeCateg;
+        this.langue = film.langue;
+        this.cote = film.cote;
+    }
+
+    public Film(int num, String titre, int codeCateg, String langue, int cote) {
+        this.num = num;
+        this.titre = titre;
+        this.codeCateg = codeCateg;
+        this.langue = langue;
+        this.cote = cote;
+    }
+
+
+    public int getNum() {
+        return num;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
+
+    public String getTitre() {
+        return titre;
+    }
+
+    public void setTitre(String titre) {
+        this.titre = titre;
+    }
+
+    public int getCodeCateg() {
+        return codeCateg;
+    }
+
+    public void setCodeCateg(int codeCateg) {
+        this.codeCateg = codeCateg;
+    }
+
+    public String getLangue() {
+        return langue;
+    }
+
+    public void setLangue(String langue) {
+        this.langue = langue;
+    }
+
+    public int getCote() {
+        return cote;
+    }
+
+    public void setCote(int cote) {
+        this.cote = cote;
+    }
+}
